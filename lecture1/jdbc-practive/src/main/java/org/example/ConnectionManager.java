a7723865fc28922718fc9371b447b6ebbdb7a16f
limchaeseong <lcs20043@gmail.com>
2024-09-10 15:36:14 +0900

    Part1)Ch05-04 실습한 JDBC 코드 리팩토링 및 DB 커넥션 풀 적용

--- lecture1/jdbc-practive/src/main/java/org/example/ConnectionManager.java
+++ lecture1/jdbc-practive/src/main/java/org/example/ConnectionManager.java
@@ -3,16 +3,54 @@ package org.example;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ConnectionManager {
 
-    public static DataSource getDataSource() {
-        HikariDataSource ds = new HikariDataSource();
-        ds.setDriverClassName("org.h2.Driver");
-        ds.setJdbcUrl("jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1");
-        ds.setUsername("sa");
-        ds.setPassword("");
+    public static final String DB_DRIVER = "org.h2.Driver";
+    public static final String DB_URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+    public static final int MAX_POOL_SIZE = 40;
 
+    private static final DataSource ds;
+
+    static {
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setDriverClassName(DB_DRIVER);
+        hikariDataSource.setJdbcUrl(DB_URL);
+        hikariDataSource.setUsername(USERNAME);
+        hikariDataSource.setPassword(PASSWORD);
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE);
+        hikariDataSource.setMinimumIdle(MAX_POOL_SIZE);
+
+        ds = hikariDataSource;
+    }
+
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+
+        // Refactoring : HikariCP를 이용한 DB Connection
+        /*
+        String url = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+        String id = "sa";
+        String pw = "";
+
+        try {
+            Class.forName("org.h2.Driver");
+            return DriverManager.getConnection(url, id, pw);
+        } catch (Exception e) {
+            return null;
+        }
+        */
+    }
+
+    public static DataSource getDataSource(){
         return ds;
     }
 }
--- lecture1/jdbc-practive/src/main/java/org/example/UserDao.java
+++ lecture1/jdbc-practive/src/main/java/org/example/UserDao.java
@@ -5,11 +5,36 @@ import java.sql.*;
 public class UserDao {
 
     public void create(User user) throws SQLException {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        String sql = "insert into users values(?,?,?,?)";
+        jdbcTemplate.executeUpdate(
+                sql,
+                pstmt -> {
+                    pstmt.setString(1, user.getUserId());
+                    pstmt.setString(2, user.getPassword());
+                    pstmt.setString(3, user.getName());
+                    pstmt.setString(4, user.getEmail());
+                }
+               /*
+                 new PreparedStatementSetter() {
+                    @Override
+                    public void setter(PreparedStatement pstmt) throws SQLException {
+                        pstmt.setString(1, user.getUserId());
+                        pstmt.setString(2, user.getPassword());
+                        pstmt.setString(3, user.getName());
+                        pstmt.setString(4, user.getEmail());
+                    }
+                }
+               */
+        );
+    }
+
+    public void oldCreate(User user) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
 
         try {
-            con = getConnection();
+            con = ConnectionManager.getConnection();
             String sql = "insert into users values(?,?,?,?)";
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, user.getUserId());
@@ -28,35 +53,36 @@ public class UserDao {
         }
     }
 
-    private Connection getConnection() {
-        String url = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
-        String id = "sa";
-        String pw = "";
+    public User findByUserId(String userId) throws SQLException {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        String sql = "select * from users where userId = ?";
 
-        try {
-            Class.forName("org.h2.Driver");
-            return DriverManager.getConnection(url, id, pw);
-        } catch (Exception e) {
-            return null;
-        }
+        return (User)jdbcTemplate.executeQuery(
+                sql,
+                pstmt -> pstmt.setString(1, userId),
+                rs -> new User(
+                        rs.getString("userId"),
+                        rs.getString("password"),
+                        rs.getString("name"),
+                        rs.getString("email"))
+        );
     }
 
-    public User findByUserId(String userId) throws SQLException {
+    public User oldFindByUserId(String userId) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
 
         try {
-            con = getConnection();
+            con = ConnectionManager.getConnection();
             String sql = "select * from users where userId = ?";
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, userId);
 
             rs = pstmt.executeQuery();
+
             User user = null;
-
             if (rs.next()) {
-
                 user = new User(
                         rs.getString("userId"),
                         rs.getString("password"),
@@ -67,9 +93,23 @@ public class UserDao {
 
             return user;
         } finally {
-            if(rs != null) rs.close();
-            if(pstmt != null) pstmt.close();
-            if(con != null) con.close();
+            if (rs != null) rs.close();
+            if (pstmt != null) pstmt.close();
+            if (con != null) con.close();
         }
     }
+
+    // Refactoring : ConnectionManager로 이동
+//    public Connection getConnection() {
+//        String url = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+//        String id = "sa";
+//        String pw = "";
+//
+//        try {
+//            Class.forName("org.h2.Driver");
+//            return DriverManager.getConnection(url, id, pw);
+//        } catch (Exception e) {
+//            return null;
+//        }
+//    }
 }
